ade893b51bd30b92cf288206d79549e8e983eb0e
Avery Wallace <aw676914@ohio.edu>
2020-03-04 10:23:17 -0500

    added controller

---
+++ Controllers/RegistersController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MIS4200Team6.DAL;
+using MIS4200Team6.Models;
+
+namespace MIS4200Team6.Controllers
+{
+    public class RegistersController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: Registers
+        public ActionResult Index()
+        {
+            return View(db.Register.ToList());
+        }
+
+        // GET: Registers/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Register register = db.Register.Find(id);
+            if (register == null)
+            {
+                return HttpNotFound();
+            }
+            return View(register);
+        }
+
+        // GET: Registers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Email,firstName,lastName,PhoneNumber,Position,hireDate")] Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                register.ID = Guid.NewGuid();
+                db.Register.Add(register);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(register);
+        }
+
+        // GET: Registers/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Register register = db.Register.Find(id);
+            if (register == null)
+            {
+                return HttpNotFound();
+            }
+            return View(register);
+        }
+
+        // POST: Registers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Email,firstName,lastName,PhoneNumber,Position,hireDate")] Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(register).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(register);
+        }
+
+        // GET: Registers/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Register register = db.Register.Find(id);
+            if (register == null)
+            {
+                return HttpNotFound();
+            }
+            return View(register);
+        }
+
+        // POST: Registers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Register register = db.Register.Find(id);
+            db.Register.Remove(register);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
