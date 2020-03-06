2d4ca303b145a9bdd6f143b7ba9cdc201f7324a0
Avery Wallace <aw676914@ohio.edu>
2020-03-06 09:53:26 -0500

    added migrations folder

--- DAL/EmployeeContext.cs
+++ DAL/EmployeeContext.cs
@@ -11,14 +11,20 @@ namespace MIS4200Team6.DAL
     {
         public EmployeeContext() : base("name=DefaultConnection")
         {
+            
             // this method is a 'constructor' and is called when a new context is created
             // the base attribute says which connection string to use
+        }
+        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
         // Include each object here. The value inside <> is the name of the class,
         // the value outside should generally be the plural of the class name
         // and is the name used to reference the entity in code
         public DbSet<Register> Register { get; set; }
-       
     }
 
 }
---
+++ Migrations/RECContext/Configuration.cs
@@ -0,0 +1,24 @@
+namespace MIS4200Team6.Migrations.RECContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MIS4200Team6.DAL.EmployeeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Migrations\RECContext";
+        }
+
+        protected override void Seed(MIS4200Team6.DAL.EmployeeContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
