b7a94b8789d3e5c9b334cd86b68720381b3af1aa
Clay Shannon <cs390914@ohio.edu>
2020-03-27 20:37:55 -0400

    added corevalue model

---
+++ Models/CoreValueLeaderBoard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MIS4200Team6.Models
+{
+    public class CoreValueLeaderBoard
+    {
+        [Key] // the data annotation is necessary because there is a field called, ID,
+              // in the table and it is not the PK for the record
+        public int leaderboardID { get; set; }
+
+        //ID of person being recognized
+        public Guid ID { get; set; }
+        [ForeignKey(name: "ID")]
+        public virtual UserDetails UserDetails { get; set; }
+        public int? Stewardship { get; set; }
+        public int? Culture { get; set; }
+        [Display(Name = "Delivery Excellence")]
+        public int? Delivery_Excellence { get; set; }
+        public int? Innovation { get; set; }
+        [Display(Name = "Greater Good")]
+        public int? Greater_Good { get; set; }
+        [Display(Name = "Integrity and Openness")]
+        public int? Integrity_And_Openness { get; set; }
+        public int? Balance { get; set; }
+        [Display(Name = "Total Points")]
+        public int? TotalPoints
+        {
+            get
+            {
+                return Stewardship + Culture + Delivery_Excellence + Innovation + Greater_Good + Integrity_And_Openness + Balance;
+            }
+        }
+
+
+    }
+}
