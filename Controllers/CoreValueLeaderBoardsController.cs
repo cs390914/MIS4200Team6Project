c7d3464c8edf970d9479697966b767e279329e42
Jakob Butler <jb213215@ohio.edu>
2020-04-16 15:36:50 -0400

    Jake Thursday 2

--- Controllers/CoreValueLeaderBoardsController.cs
+++ Controllers/CoreValueLeaderBoardsController.cs
@@ -14,8 +14,17 @@ namespace MIS4200Team6.Controllers
         // GET: CoreValueLeaderBoards
         public ActionResult Index()
         {
-            var coreValueLeaderBoards = db.coreValueLeaderBoards.Include(c => c.Registrar);
-            return View(coreValueLeaderBoards.ToList());
+            if (User.Identity.IsAuthenticated)
+            {
+                var coreValueLeaderBoards = db.coreValueLeaderBoards.Include(c => c.Registrar);
+                return View(coreValueLeaderBoards.ToList());
+            }
+            
+            else
+            {
+                return View("NotAuthenticated");
+            }
+            
         }
 
         // GET: CoreValueLeaderBoards/Details/5
