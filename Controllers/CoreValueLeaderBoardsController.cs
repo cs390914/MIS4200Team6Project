7b516589e2c8b2651cf64d643fb919c5a89f00a4
Jakob Butler <jb213215@ohio.edu>
2020-04-21 19:54:12 -0400

    Jake Tuesday

--- Controllers/CoreValueLeaderBoardsController.cs
+++ Controllers/CoreValueLeaderBoardsController.cs
@@ -4,6 +4,9 @@ using System.Net;
 using System.Web.Mvc;
 using MIS4200Team6.DAL;
 using MIS4200Team6.Models;
+using System.Net;
+using System.Net.Mail;
+using System;
 
 namespace MIS4200Team6.Controllers
 {
@@ -45,7 +48,7 @@ namespace MIS4200Team6.Controllers
         // GET: CoreValueLeaderBoards/Create
         public ActionResult Create()
         {
-            ViewBag.ID = new SelectList(db.Register, "ID", "Email");
+            ViewBag.ID = new SelectList(db.Register, "ID", "EmailAddress");
             return View();
         }
 
@@ -60,10 +63,43 @@ namespace MIS4200Team6.Controllers
             {
                 db.coreValueLeaderBoards.Add(coreValueLeaderBoard);
                 db.SaveChanges();
+
+                SmtpClient myClient = new SmtpClient();
+                // the following line has to contain the email address and password of someone
+                // authorized to use the email server (you will need a valid Ohio account/password
+                // for this to work)
+                myClient.Credentials = new NetworkCredential("AuthorizedUser", "UserPassword");
+                MailMessage myMessage = new MailMessage();
+                // the syntax here is email address, username (that will appear in the email)
+                MailAddress from = new MailAddress("jb213215@ohio.edu", "SysAdmin");
+                myMessage.From = from;
+                myMessage.To.Add("jakebutler67@gmail.com"); // this should be replaced with model data
+                                                            // as shown at the end of this document
+                myMessage.Subject = "MVC Email test";
+                // the body of the email is hard coded here but could be dynamically created using data
+                // from the model- see the note at the end of this document
+                myMessage.Body = "This is the body of the mail message. This can be essentially any length, and could come ";
+                myMessage.Body += "from the database, a variable, the return of another method...";
+                try
+                {
+                    myClient.Send(myMessage);
+                    TempData["mailError"] = "";
+                }
+                catch (Exception ex)
+
+                {
+                    // this captures an Exception and allows you to display the message in the View
+                    TempData["mailError"] = ex.Message;
+                }
+
                 return RedirectToAction("Index");
             }
 
-            ViewBag.ID = new SelectList(db.Register, "ID", "Email", coreValueLeaderBoard.ID);
+            ViewBag.ID = new SelectList(db.Register, "ID", "EmailAddress", coreValueLeaderBoard.ID);
+            
+
+
+         
             return View(coreValueLeaderBoard);
         }
 
@@ -79,7 +115,7 @@ namespace MIS4200Team6.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.ID = new SelectList(db.Register, "ID", "Email", coreValueLeaderBoard.ID);
+            ViewBag.ID = new SelectList(db.Register, "ID", "EmailAddress", coreValueLeaderBoard.ID);
             return View(coreValueLeaderBoard);
         }
 
@@ -96,7 +132,7 @@ namespace MIS4200Team6.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.ID = new SelectList(db.Register, "ID", "Email", coreValueLeaderBoard.ID);
+            ViewBag.ID = new SelectList(db.Register, "ID", "EmailAddress", coreValueLeaderBoard.ID);
             return View(coreValueLeaderBoard);
         }
 
